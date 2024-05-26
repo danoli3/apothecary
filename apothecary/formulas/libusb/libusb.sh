2b7c44d46e62749a508b40f80eabeaad9d853d8d
Dan Rosser <danoli3@gmail.com>
2024-05-26 23:55:37 +1000

    libusb vs fix

--- apothecary/formulas/libusb/libusb.sh
+++ apothecary/formulas/libusb/libusb.sh
@@ -142,7 +142,6 @@ function copy() {
 	. "$SECURE_SCRIPT"
 	if [ "$TYPE" == "vs" ] ; then
 		mkdir -p $1/lib/$TYPE/$PLATFORM/
-		cp -Rv "build_${TYPE}_${PLATFORM}/Release/include/" $1/ 
     	cp -f "build_${TYPE}_${PLATFORM}/Release/libusb-1.0.dll" $1/lib/$TYPE/$PLATFORM/libusb.dll
     	cp -f "build_${TYPE}_${PLATFORM}/Release/usb-1.0.lib" $1/lib/$TYPE/$PLATFORM/libusb.lib
 		secure $1/lib/$TYPE/$PLATFORM/libusb.lib libusb
