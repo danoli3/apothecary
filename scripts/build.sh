16fa9ce2c39e4b4ab6c94233b826053ea3cd4d03
Dan Rosser <danoli3@gmail.com>
2024-07-24 21:26:40 +1000

    Emscripten make sure to pipe ci variable

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,8 +9,21 @@ else
     export FORCE=""
 fi
 
+if [ -z "${PTHREADS_ENABLED+x}" ]; then
+    export PTHREADS_ENABLED=0
+fi
 
+# Print the value to verify it's set
+echo "PTHREADS_ENABLED is set to: $PTHREADS_ENABLED"
 
+# Your existing build logic here
+
+# Example of using the variable
+if [ "$PTHREADS_ENABLED" -eq 1 ]; then
+    echo "PThreads is enabled"
+else
+    echo "PThreads is not enabled"
+fi
 
 # trap any script errors and exit
 # trap "trapError" ERR
