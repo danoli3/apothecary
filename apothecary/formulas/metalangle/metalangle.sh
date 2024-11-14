34425a0b6eb426cf5cb30f40711ffebba69ac6e8
Dan Rosser <danoli3@gmail.com>
2024-11-14 15:34:44 +1100

    merge
    (cherry picked from commit 3112874ca3722ff38b9fb7157a72b91018d9f433)

--- apothecary/formulas/metalangle/metalangle.sh
+++ apothecary/formulas/metalangle/metalangle.sh
@@ -19,6 +19,10 @@ GIT_TAG=v$VER
 SCHEME=MetalANGLE
 ARCHS=~/Library/Developer/Xcode/Archives
 
+BUILD_CMAKE=false
+BUILD_XCARCHIVE=false
+BUILD_STATIC=true
+
 # download the source code and unpack it into LIB_NAME
 function download() {
 	. "$DOWNLOADER_SCRIPT"
@@ -58,31 +62,134 @@ function build() {
 		    -DBUILD_SHARED_LIBS=OFF"
 
 	if [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
+
+		# rm -f build_${TYPE}_${PLATFORM}
+
+		XC_PROJECT_PATH="./ios/xcode/OpenGLES.xcodeproj"
+
 		mkdir -p "build_${TYPE}_${PLATFORM}"
 		cd "build_${TYPE}_${PLATFORM}"
-		rm -f CMakeCache.txt *.a *.o
 
-		cmake .. ${DEFS} \
-				-DCMAKE_TOOLCHAIN_FILE=$APOTHECARY_DIR/toolchains/ios.toolchain.cmake \
-				-DPLATFORM=$PLATFORM \
-				-DCMAKE_INSTALL_PREFIX=Release \
-				-DCMAKE_BUILD_TYPE=Release \
-				-DDEPLOYMENT_TARGET=${MIN_SDK_VER} \
-				-DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
-				-DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
-				-DENABLE_BITCODE=OFF \
-				-DENABLE_ARC=ON \
-				-DENABLE_VISIBILITY=OFF \
-				-DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
-				-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
-		cmake --build . --config Release --target install
-		cd ..
-	fi
-    # if [ "$TYPE" == "ios" ] ; then
-	# 	xcodebuild archive -scheme $SCHEME -archivePath $ARCHS/iOSDevice.xcarchive -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project OpenGLES.xcodeproj
-	# elif [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
-	# 	echo
-	# fi
+		if [[ $BUILD_CMAKE == true ]]; then
+
+			rm -f CMakeCache.txt *.a *.o
+			if [[ "$TYPE" =~ ^(catos)$ ]]; then
+				X_DEFS="-DSUPPORTS_MACCATALYST=YES "
+			else
+				X_DEFS=""
+			fi
+			if [[ "$TYPE" =~ ^(ios)$ ]]; then
+				X_LINKER=" -framework OpenGLES -framework IOSurface "
+			else
+				X_LINKER=" -framework OpenGLES -framework IOSurface"
+			fi
+			cmake .. ${DEFS} ${X_DEFS} \
+					-DCMAKE_TOOLCHAIN_FILE=$APOTHECARY_DIR/toolchains/ios.toolchain.cmake \
+					-DPLATFORM=$PLATFORM \
+					-DCMAKE_INSTALL_PREFIX=Release \
+					-DCMAKE_BUILD_TYPE=Release \
+					-DDEPLOYMENT_TARGET=${MIN_SDK_VER} \
+					-DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+					-DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+					-DCMAKE_EXE_LINKER_FLAGS="-framework QuartzCore -framework Metal -framework CoreFoundation -framework Foundation -framework UIKIT -framework CoreGraphics ${X_LINKER}" \
+					-DENABLE_BITCODE=OFF \
+					-DENABLE_ARC=ON \
+					-DENABLE_VISIBILITY=OFF \
+					-DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
+					-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
+			cmake --build . --config Release --target install
+			cd ..
+
+		fi
+
+
+		ARCHS=Release
+		mkdir -p $ARCHS
+
+		XC_PROJECT_PATH="../ios/xcode/OpenGLES.xcodeproj"
+		XC_PROJECT_PATH=$(realpath $XC_PROJECT_PATH)
+
+		SCHEME=MetalANGLE
+		SCHEME_MAC=MetalANGLE_mac
+		SCHEME_TV=MetalANGLE_tvos
+		SCHEME_VISION=MetalANGLE_xros
+
+		BUILD_DIR=$(pwd)
+
+		if [[ $BUILD_XCARCHIVE == true ]]; then
+
+			if [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
+			    # Ensure we're using the correct Xcode project path
+			    if [[ -d "$XC_PROJECT_PATH" ]]; then
+			        # For each platform type, run the xcodebuild command with XC_PROJECT_PATH
+			        if [[ "$TYPE" =~ ^(xros)$ ]]; then
+				        echo "*** Creating archive for visionOS ***"
+				        xcodebuild archive -scheme $SCHEME_VISION -archivePath $ARCHS/MetalANGLE.xcarchive -sdk xros SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				        echo "*** Creating archive for visionOS Simulator ***"
+				        xcodebuild archive -scheme $SCHEME_VISION -archivePath $ARCHS/MetalANGLE.xcarchive -sdk xrsimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				    elif [[ "$TYPE" =~ ^(tvos)$ ]]; then
+				        echo "*** Creating archive for tvOS ***"
+				        xcodebuild archive -scheme $SCHEME_TV -archivePath $ARCHS/MetalANGLE.xcarchive -sdk appletvos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				        echo "*** Creating archive for tvOS Simulator ***"
+				        xcodebuild archive -scheme $SCHEME_TV -archivePath $ARCHS/MetalANGLE.xcarchive -sdk appletvsimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				    elif [[ "$TYPE" =~ ^(ios)$ ]]; then
+				        echo "*** Creating archive for iOS ***"
+				        xcodebuild archive -scheme $SCHEME -archivePath $ARCHS/MetalANGLE.xcarchive -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				        echo "*** Creating archive for iOS Simulator ***"
+				        xcodebuild archive -scheme $SCHEME -archivePath $ARCHS/MetalANGLE.xcarchive -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				    elif [[ "$TYPE" =~ ^(catos)$ ]]; then
+				        echo "*** Creating archive for Mac Catalyst ***"
+				        xcodebuild archive -scheme $SCHEME -archivePath $ARCHS/MetalANGLE.xcarchive -destination "generic/platform=macOS,variant=Mac Catalyst" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES -project "${XC_PROJECT_PATH}"
+				    elif [[ "$TYPE" =~ ^(osx)$ ]]; then
+				        echo "*** Creating archive for macOS ***"
+				        xcodebuild archive -scheme $SCHEME_MAC -archivePath $ARCHS/MetalANGLE.xcarchive -destination "generic/platform=macOS,name=Any Mac" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}"
+				    fi
+			    else
+			        echo "Error: Xcode project not found at $XC_PROJECT_PATH"
+			        exit 1
+			    fi
+			fi
+
+		fi
+
+		if [[ $BUILD_STATIC == true ]]; then
+
+			if [[ "$TYPE" =~ ^(xros)$ ]]; then
+	            # visionOS static library build
+	            echo "*** Building static library for visionOS ***"
+	            xcodebuild clean build -scheme $SCHEME_VISION -configuration Release -sdk xros BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+	            echo "*** Building static library for visionOS Simulator ***"
+	            xcodebuild clean build -scheme $SCHEME_VISION -configuration Release -sdk xrsimulator BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+
+	        elif [[ "$TYPE" =~ ^(tvos)$ ]]; then
+	            # tvOS static library build
+	            echo "*** Building static library for tvOS ***"
+	            xcodebuild clean build -scheme $SCHEME_TV -configuration Release -sdk appletvos BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+	            echo "*** Building static library for tvOS Simulator ***"
+	            xcodebuild clean build -scheme $SCHEME_TV -configuration Release -sdk appletvsimulator BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+
+	        elif [[ "$TYPE" =~ ^(ios)$ ]]; then
+	            # iOS static library build
+	            echo "*** Building static library for iOS ***"
+	            xcodebuild clean build -scheme $SCHEME -configuration Release -sdk iphoneos BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+	            echo "*** Building static library for iOS Simulator ***"
+	            xcodebuild clean build -scheme $SCHEME -configuration Release -sdk iphonesimulator BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+
+	        elif [[ "$TYPE" =~ ^(catos)$ ]]; then
+	            # Mac Catalyst static library build
+	            echo "*** Building static library for Mac Catalyst ***"
+	            xcodebuild clean build -scheme $SCHEME -configuration Release -destination "generic/platform=macOS,variant=Mac Catalyst" BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+
+	        elif [[ "$TYPE" =~ ^(osx)$ ]]; then
+	            # macOS static library build
+	            echo "*** Building static library for macOS ***"
+	            xcodebuild clean build -scheme $SCHEME_MAC -configuration Release -destination "generic/platform=macOS,name=Any Mac" BUILD_LIBRARIES_FOR_DISTRIBUTION=YES -project "${XC_PROJECT_PATH}" -derivedDataPath "$BUILD_DIR"
+	        fi
+
+    	fi
+
+    fi
+
 
 }
 
@@ -97,11 +204,17 @@ function copy() {
     . "$SECURE_SCRIPT"
     # libs
     mkdir -p $1/lib/$TYPE
+
     if [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
         cp -v -r build_${TYPE}_${PLATFORM}/Release/include/* $1/include
         mkdir -p $1/lib/$TYPE/$PLATFORM/
-        cp -Rv build_${TYPE}_${PLATFORM}/Release/lib/libmetalangle.a $1/lib/$TYPE/$PLATFORM/libmetalangle.a
-        secure $1/lib/$TYPE/$PLATFORM/libmetalangle.a metalangle.pkl
+        if [[ $BUILD_STATIC == true ]]; then
+        	cp -Rv build_${TYPE}_${PLATFORM}/Release/lib/libmetalangle.a $1/lib/$TYPE/$PLATFORM/MetalANGLE.a
+       		secure $1/lib/$TYPE/$PLATFORM/metalangle.a metalangle.pkl
+    	fi
+    	if [[ $BUILD_XCARCHIVE == true ]]; then
+    		cp -Rv build_${TYPE}_${PLATFORM}/Release/MetalANGLE.xcarchive $1/lib/$TYPE/$PLATFORM/MetalANGLE.xcarchive
+    	fi
     fi
 
     # copy license files
