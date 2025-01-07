31b5d1c78ee8de679de8dd7cf18b32e6ccd4caf8
Dan Rosser <danoli3@gmail.com>
2025-01-08 03:14:42 +1100

    link

--- scripts/linuxaarch64/install_and_build.sh
+++ scripts/linuxaarch64/install_and_build.sh
@@ -49,9 +49,13 @@ sudo chmod +x SSymlinker
 # ./SSymlinker -s ${LIBC_USR}/include/sound -d /usr/include
 ./SSymlinker -s ${LIBC_USR}/include/video -d /usr/include
 ./SSymlinker -s ${LIBC_USR}/include -d /usr/include
-./SSymlinker -s ${CRT}/crtn.o -d /usr/lib64/crtn.o
-./SSymlinker -s ${CRT}/crt1.o -d /usr/lib64/crt1.o
-./SSymlinker -s ${CRT}/crti.o -d /usr/lib64/crti.o
+./SSymlinker -s ${CRT}/crtn.o -d /usr/lib/crtn.o
+./SSymlinker -s ${CRT}/crt1.o -d /usr/lib/crt1.o
+./SSymlinker -s ${CRT}/crti.o -d /usr/lib/crti.o
+
+ln -sf ${CRT}/crti.o /usr/lib/crti.o
+ln -sf ${CRT}/crt1.o /usr/lib/crt1.o
+ln -sf ${CRT}/crtn.o /usr/lib/crtn.o
 
 echo 'export PATH=$PATH' >> .bashrc
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH' >> .bashrc
--- scripts/linuxarmv6l/install_and_build.sh
+++ scripts/linuxarmv6l/install_and_build.sh
@@ -54,6 +54,10 @@ sudo chmod +x SSymlinker
 ./SSymlinker -s ${CRT}/crt1.o -d /usr/lib/crt1.o
 ./SSymlinker -s ${CRT}/crti.o -d /usr/lib/crti.o
 
+ln -sf ${CRT}/crti.o /usr/lib/crti.o
+ln -sf ${CRT}/crt1.o /usr/lib/crt1.o
+ln -sf ${CRT}/crtn.o /usr/lib/crtn.o
+
 echo 'export PATH=$PATH' >> .bashrc
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH' >> .bashrc
 source .bashrc
