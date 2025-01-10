60eaa9000c26dace99e59a57edf260561987364f
Dan Rosser <danoli3@gmail.com>
2025-01-10 14:10:49 +1100

    Toolchain Linux Fixes

--- apothecary/toolchains/linuxaarch64-clang.toolchain.cmake
+++ apothecary/toolchains/linuxaarch64-clang.toolchain.cmake
@@ -4,6 +4,8 @@ set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
+# Raspberry Pi - 3 Model A+/B+ & 4 Model B 400/B & 5 & Compute 3/3-lite/3+/4 (64-Bit)
+
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
 set(GCC_VERSION 14.2.0)
@@ -14,10 +16,15 @@ endif()
 if(NOT DEFINED CPP_STANDARD)
     set(CPP_STANDARD 17) # Default to C++17
 endif()
+
 set(CMAKE_C_STANDARD ${C_STANDARD})
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_STANDARD ${CPP_STANDARD})
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if(NOT DEFINED M_CPU)
+    set(M_CPU cortex-a53) # Default to cortex-a53 / cortex-A76
+endif()
 
 set(tools ${TOOLCHAIN_ROOT}) # warning change toolchain path here.
 set(rootfs_dir ${SYSROOT}/rootfs) # warning change compiled rootfs path here.
@@ -25,18 +32,20 @@ set(rootfs_dir ${SYSROOT}/rootfs) # warning change compiled rootfs path here.
 set(CMAKE_FIND_ROOT_PATH ${rootfs_dir})
 set(CMAKE_SYSROOT ${rootfs_dir})
 
-set(EXTRA_LINKS "-Wl,-rpath-link,${CMAKE_SYSROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/lib64 \
-    -L${CMAKE_SYSROOT}/lib \
-    -L/usr/lib/ \
-    -L${CMAKE_SYSROOT}/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_VERSION} \
-    -L${CMAKE_SYSROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/lib64 \
-    -L${CMAKE_SYSROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/usr/lib64")
+set(EXTRA_LINKS "-Wl, -rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} \
+    -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} \
+    -L${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/lib64 \
+    -rpath-link,${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/lib64 \
+    -L${TOOLCHAIN_ROOT}/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_VERSION} \
+    -L${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/lib64 \
+    -L${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/usr/lib64 \
+    -rpath-link,${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/usr/lib64")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fPIC ${EXTRA_LINKS}")
 
-set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fPIC ${EXTRA_LINKS} -mcpu=cortex-a53")
+set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fPIC ${EXTRA_LINKS} -mcpu=${M_CPU} -march=armv8-a+fp+simd")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC ${EXTRA_LINKS} -mcpu=cortex-a53")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC ${EXTRA_LINKS} -mcpu=${M_CPU} -march=armv8-a+fp+simd")
 
 ## Compiler Binary 
 set(BIN_PREFIX ${TOOLCHAIN_ROOT}/bin/${CMAKE_LIBRARY_ARCHITECTURE})
--- apothecary/toolchains/linuxarmv8-clang.toolchain.cmake
+++ apothecary/toolchains/linuxarmv8-clang.toolchain.cmake
@@ -4,8 +4,8 @@ set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
-# Change to ARMv6
-set(CMAKE_SYSTEM_PROCESSOR armv6)
+# Change to Raspberry Pi - 3 Model A+/B+ & 4 Model 400/B & 5 & Compute 3/3-lite/3+/4 (32-Bit)
+set(CMAKE_SYSTEM_PROCESSOR armv8)
 set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
 set(GCC_VERSION 14.2.0)
 
@@ -21,14 +21,10 @@ endif()
 if(NOT DEFINED CPP_STANDARD)
     set(CPP_STANDARD 17) # Default to C++17
 endif()
-
 set(CMAKE_C_STANDARD ${C_STANDARD})
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_STANDARD ${CPP_STANDARD})
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-# Compiler Binary
-set(BIN_PREFIX ${TOOLCHAIN_ROOT}/bin/${CMAKE_LIBRARY_ARCHITECTURE})
 
 set(EXTRA_LINKS "-Wl, -rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} \
     -L${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE} \
@@ -40,14 +36,15 @@ set(EXTRA_LINKS "-Wl, -rpath-link,${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHI
     -L${TOOLCHAIN_ROOT}/${CMAKE_LIBRARY_ARCHITECTURE}/libc/usr/lib \
     -L${TOOLCHAIN_ROOT}/lib/gcc/${CMAKE_LIBRARY_ARCHITECTURE}/${GCC_VERSION}")
 
-# Update linker flags for ARMv7
+# Update linker flags for armv8-a
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fPIC ${EXTRA_LINKS}")
 
-# Update compiler flags for ARMv7
-set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -march=armv6 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard ${EXTRA_LINKS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -march=armv6 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard ${EXTRA_LINKS}")
+# Update compiler flags for armv8-a
+set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8 ${EXTRA_LINKS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -march=armv8-a -mfloat-abi=hard -mfpu=neon-fp-armv8 ${EXTRA_LINKS}")
 
-
+# Compiler Binary
+set(BIN_PREFIX ${TOOLCHAIN_ROOT}/bin/${CMAKE_LIBRARY_ARCHITECTURE})
 
 set(CMAKE_C_COMPILER ${BIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${BIN_PREFIX}-g++)
