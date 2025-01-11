2bbbe9b844443ff3c9c17d19a3773fd75065b57a
Dan Rosser <danoli3@gmail.com>
2025-01-11 21:52:24 +1100

    kiss

--- apothecary/formulas/kiss/kiss.sh
+++ apothecary/formulas/kiss/kiss.sh
@@ -30,48 +30,48 @@ function prepare() {
 # executed inside the lib src dir
 function build() {
     
-    if [ "$TYPE" == "linux" ]; then
-	    if [ $CROSSCOMPILING -eq 1 ]; then
-            source $APOTHECARY_DIR/configure/${TYPE}${PLATFORM}_configure.sh
-        fi
-		echo "building $TYPE | $PLATFORM"
-        echo "--------------------"
-		mkdir -p "build_${TYPE}_${PLATFORM}"
-		cd "build_${TYPE}_${PLATFORM}"
+    # if [ "$TYPE" == "linux" ]; then
+	#     if [ $CROSSCOMPILING -eq 1 ]; then
+    #         source $APOTHECARY_DIR/configure/${TYPE}${PLATFORM}_configure.sh
+    #     fi
+	# 	echo "building $TYPE | $PLATFORM"
+    #     echo "--------------------"
+	# 	mkdir -p "build_${TYPE}_${PLATFORM}"
+	# 	cd "build_${TYPE}_${PLATFORM}"
 
-		echo "TOOLCHAIN_ROOT is set to: ${TOOLCHAIN_ROOT}"
-	    rm -f CMakeCache.txt *.a *.o *.so
+	# 	echo "TOOLCHAIN_ROOT is set to: ${TOOLCHAIN_ROOT}"
+	#     rm -f CMakeCache.txt *.a *.o *.so
 	 
-	    DEFINES="${DEFINES} -DLIBRARY_SUFFIX=${ARCH} \
-	        -DCMAKE_BUILD_TYPE=Release \
-	        -DCMAKE_C_STANDARD=${C_STANDARD} \
-	        -DCMAKE_CXX_STANDARD=${CPP_STANDARD} \
-	        -DCMAKE_CXX_STANDARD_REQUIRED=ON \
-	        -DCMAKE_CXX_EXTENSIONS=OFF
-	        -DBUILD_SHARED_LIBS=OFF"
-	    cmake .. ${DEFINES} \
-			-DCMAKE_TOOLCHAIN_FILE=$APOTHECARY_DIR/toolchains/${TYPE}${PLATFORM}.toolchain.cmake \
-			-DGCC_VERSION=${GCC_VERSION} \
-			-DCMAKE_SYSTEM_PROCESSOR=$ABI \
-	        -DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
-	        -DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
-	        -DCMAKE_BUILD_TYPE=Release \
-	        -DCMAKE_INSTALL_LIBDIR="lib" \
-	        -DCMAKE_SYSTEM_NAME=$TYPE \
-	        -DCMAKE_INSTALL_PREFIX=Release \
-            -DCMAKE_INCLUDE_OUTPUT_DIRECTORY=include \
-            -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
-            -DENABLE_VISIBILITY=OFF \
-            -DCMAKE_INSTALL_INCLUDEDIR=include \
-            -DCMAKE_VERBOSE_MAKEFILE=true
-	    cmake --build . --target install --config Release -j${PARALLEL_MAKE}
-	    cd ..
-	else
+	#     DEFINES="${DEFINES} -DLIBRARY_SUFFIX=${ARCH} \
+	#         -DCMAKE_BUILD_TYPE=Release \
+	#         -DCMAKE_C_STANDARD=${C_STANDARD} \
+	#         -DCMAKE_CXX_STANDARD=${CPP_STANDARD} \
+	#         -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+	#         -DCMAKE_CXX_EXTENSIONS=OFF
+	#         -DBUILD_SHARED_LIBS=OFF"
+	#     cmake .. ${DEFINES} \
+	# 		-DCMAKE_TOOLCHAIN_FILE=$APOTHECARY_DIR/toolchains/${TYPE}${PLATFORM}.toolchain.cmake \
+	# 		-DGCC_VERSION=${GCC_VERSION} \
+	# 		-DCMAKE_SYSTEM_PROCESSOR=$ABI \
+	#         -DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+	#         -DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+	#         -DCMAKE_BUILD_TYPE=Release \
+	#         -DCMAKE_INSTALL_LIBDIR="lib" \
+	#         -DCMAKE_SYSTEM_NAME=$TYPE \
+	#         -DCMAKE_INSTALL_PREFIX=Release \
+    #         -DCMAKE_INCLUDE_OUTPUT_DIRECTORY=include \
+    #         -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
+    #         -DENABLE_VISIBILITY=OFF \
+    #         -DCMAKE_INSTALL_INCLUDEDIR=include \
+    #         -DCMAKE_VERBOSE_MAKEFILE=true
+	#     cmake --build . --target install --config Release -j${PARALLEL_MAKE}
+	#     cd ..
+	# else
 		if [ $CROSSCOMPILING -eq 1 ]; then
         	source $APOTHECARY_DIR/configure/${TYPE}${PLATFORM}_configure.sh
     	fi
     	make  -j${PARALLEL_MAKE} TARGET_DIR=$TYPE
-	fi
+	# fi
 }
 
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
