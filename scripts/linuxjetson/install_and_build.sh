5514c053d259aed83ff93485f479fddc8a2d68aa
Dan Rosser <danoli3@gmail.com>
2025-01-12 04:02:45 +1100

    Install linux cross

--- scripts/linuxjetson/install_and_build.sh
+++ scripts/linuxjetson/install_and_build.sh
@@ -11,8 +11,10 @@ trapError() {
 	exit 1
 }
 
-sudo apt-get install -y aptitude build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev cmake unzip pkg-config flex openssl pigz autoconf automake tar figlet xz-utils
+sudo apt-get install -y aptitude build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev cmake unzip pkg-config flex openssl pigz autoconf automake tar figlet xz-utils libtool dos2unix
+sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libxrandr-dev libxinerama-dev libx11-dev libxext-dev libxcursor-dev libxi-dev ccache
 sudo aptitude install -y gperf
+
 
 echo "calculate formulas"
 $ROOT/scripts/calculate_formulas.sh
