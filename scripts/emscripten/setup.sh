50f91afa3e5965164ab3c841cea42637fe84fcd1
Dan Rosser <danoli3@gmail.com>
2025-02-20 19:13:33 +1100

    EMSDK fix

--- scripts/emscripten/setup.sh
+++ scripts/emscripten/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-EMSDK_PATH="$HOME/emsdk"
+EMSDK=${EMSDK:-$HOME/emsdk}
 EMSDK_VERSION=${EMSDK_VERSION:-latest} # "3.1.20"
 
 check_emsdk() {
@@ -17,17 +17,17 @@ check_emsdk() {
     fi
 }
 
-if check_emsdk "$EMSDK_PATH"; then
-    echo "Emscripten SDK already installed at $EMSDK_PATH"
+if check_emsdk "$EMSDK"; then
+    echo "Emscripten SDK already installed at $EMSDK"
 else
     echo "===Emscripten SDK not found. Installing...==="
-    if [ ! -d "$EMSDK_PATH" ]; then
-        git clone https://github.com/emscripten-core/emsdk.git "$EMSDK_PATH"
+    if [ ! -d "$EMSDK" ]; then
+        git clone https://github.com/emscripten-core/emsdk.git "$EMSDK"
     else
-        cd "$EMSDK_PATH"
+        cd "$EMSDK"
         git pull
     fi
-    cd "$EMSDK_PATH"
+    cd "$EMSDK"
     ./emsdk install "$EMSDK_VERSION"
     ./emsdk activate "$EMSDK_VERSION"
 
@@ -44,7 +44,7 @@ else
         echo 'export PATH="$HOME/emsdk:$HOME/emsdk/upstream/emscripten:$PATH"' >> "$HOME/.zshrc"
     fi
 
-    if check_emsdk "$EMSDK_PATH"; then
+    if check_emsdk "$EMSDK"; then
         echo "Emscripten SDK installed successfully."
     else
         echo "Error: Failed to install Emscripten SDK."
@@ -52,4 +52,4 @@ else
     fi
 fi
 
-source "$EMSDK_PATH/emsdk_env.sh"
+source "$EMSDK/emsdk_env.sh"
