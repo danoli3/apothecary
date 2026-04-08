899d8b8c905651ed3bc6bcb27d8979e5a93757f1
Dan Rosser <danoli3@gmail.com>
2026-04-08 13:45:56 +1000

    GitHub Actions Updates (#548)
    * GitHub Actions Updates and MetalAngle Enabled. xros/catOS/watchOS enabled
    
    * Actions Update macOS-14 runner still
    
    * Metal Angle off for now their download depends is broken
    
    * macOS Fix modular
    
    * Metal Angle changes
    
    * macOS Actions
    
    * utf8 4.0.6 -> 4.0.9

--- apothecary/formulas/utf8.sh
+++ apothecary/formulas/utf8.sh
@@ -7,9 +7,9 @@ FORMULA_TYPES=("osx" "msys2" "linux" "vs" "ios" "watchos" "catos" "xros" "tvos" 
 FORMULA_DEPENDS=()
 
 # define the version
-VER=4.0.6
-VER_=4_0_6
-BUILD_ID=2
+VER=4.0.9
+VER_=4_0_9
+BUILD_ID=3
 DEFINES=""
 
 # tools for git use
